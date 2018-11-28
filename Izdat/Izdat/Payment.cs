0a08d9dce01611b291aee0955459bfee7aafefea
Veronika Linkova <v.a.krilova@yandex.ru>
2018-11-28 22:01:27 +0300

    OOP1

---
+++ Izdat/Izdat/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izdat
+{
+    public class Payment
+    {
+        private DateTime date;
+        private decimal sum;
+
+        public Payment (DateTime date, decimal sum)
+        {
+            this.date = date;
+            this.sum = sum;
+        }
+    }
+}
